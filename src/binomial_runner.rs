3a1091b8ccde5e7af2d150e8ab41bae048807bfc
mwagner6 <max.wagner.us@gmail.com>
2025-05-07 19:21:29 -0400

    variable p and test cases

--- src/binomial_runner.rs
+++ src/binomial_runner.rs
@@ -30,6 +30,13 @@ pub struct BinomialRunner {
     lhs: RistrettoPoint,
     rhs: RistrettoPoint,
     result_output: Scalar,
+    var_p_in_count: i32,
+    var_p_n: i32,
+    var_p_bits: Vec<Vec<Scalar>>,
+    var_p_coms: Vec<Vec<RistrettoPoint>>,
+    var_p_randomness: Vec<Vec<Scalar>>,
+    var_p_randomness_final: Vec<Scalar>,
+    var_p: bool,
 }
 
 impl BinomialRunner {
@@ -99,7 +106,14 @@ impl BinomialRunner {
             final_z: Scalar::zero(),
             lhs: RistrettoPoint::default(),
             rhs: RistrettoPoint::default(),
-            result_output: Scalar::zero()
+            result_output: Scalar::zero(),
+            var_p_in_count: 0,
+            var_p_n: 0,
+            var_p_bits: Vec::new(),
+            var_p_coms: Vec::new(),
+            var_p_randomness: Vec::new(),
+            var_p_randomness_final: Vec::new(),
+            var_p: false,
         }
     }
 
@@ -179,6 +193,65 @@ impl BinomialRunner {
         ).collect()
     }
 
+    // <===== Alternate Step 7 =====>
+    // Given ranges of values that are privately 0 or 1, returns a list of indices, and records the commitments of each. 
+    // Used for variable p binomial mechanism
+    pub fn rand_p_init(&mut self, n: i32) {
+        self.var_p_n = n;
+        self.var_p = true;
+    }
+
+    pub fn random_variable_p_input(&mut self, k: u32, m: i32, bits: &[u8]) -> bool {
+        if bits.len() != m as usize {
+            return false;
+        }
+
+        let bits: Vec<Scalar> = bits.iter().map(|b| {
+            match *b {
+                0 => {
+                    Scalar::zero()
+                },
+                _ => {
+                    Scalar::one()
+                }
+            }
+        }).collect();
+
+        let r: Vec<Scalar> = (0..bits.len()).map(|_| {
+            let mut csprng = OsRng;
+                Scalar::random(&mut csprng)
+        }).collect();
+
+        let bit_coms: Vec<RistrettoPoint> = bits.iter().zip(r.iter()).map(|(b, r)| self.server.com.commit(*b, *r)).collect();
+        let lhs: RistrettoPoint = bit_coms.iter().sum();
+        let rhs = self.server.com.commit(Scalar::try_from(k).unwrap(), r.iter().sum());
+        if lhs != rhs {
+            return false;
+        } else {
+            self.var_p_bits.push(bits);
+            self.var_p_coms.push(bit_coms);
+            self.var_p_randomness.push(r);
+            self.var_p_in_count += 1;
+            return true;
+        }
+        
+    }
+
+    pub fn random_variable_p_end(&mut self) -> bool {
+        if self.var_p_n != self.var_p_in_count {
+            false
+        } else {
+            for range in self.var_p_bits.iter().zip(self.var_p_coms.iter()).zip(self.var_p_randomness.iter()) {
+                let ind = (rand::random::<f32>() * range.0.0.len() as f32).floor() as usize;
+                self.xor_bits.push(range.0.0[ind]);
+                self.xor_commits.push(range.0.1[ind]);
+                self.var_p_randomness_final.push(range.1[ind])
+            }
+            true
+        }
+    }
+
+
     // <===== Step 8 =====>
     // XOR Private and public bits. This is done, so this call will return XORed bits and their commits
     pub fn get_xor_bits(&self) -> Vec<u8> {
@@ -214,15 +287,25 @@ impl BinomialRunner {
     // <===== Step 9 =====>
     // Compute sum (output result). 
     pub fn compute_sum(&mut self) -> u64 {
-        let v_sum: Scalar = self.public_bits.iter().sum();
-        let s_sum: Scalar = self.xor_bits.iter().sum();
-        let x = self.x_sum + v_sum;
-        let z = self.r_sum + s_sum;
-        self.result_output = self.x_sum + s_sum;
-        self.final_x = x;
-        self.final_z = z;
+        if self.var_p {
+            let p_sum: Scalar = self.xor_bits.iter().sum();
+            self.result_output = self.x_sum + p_sum;
+            let s_sum: Scalar = self.var_p_randomness_final.iter().sum();
+            self.final_x = self.x_sum + p_sum;
+            self.final_z = self.r_sum + s_sum;
 
-        BigUint::from_bytes_le(&self.result_output.to_bytes()).to_u64_digits()[0]
+            BigUint::from_bytes_le(&self.result_output.to_bytes()).to_u64_digits()[0]
+        } else {
+            let v_sum: Scalar = self.public_bits.iter().sum();
+            let s_sum: Scalar = self.xor_bits.iter().sum();
+            let x = self.x_sum + v_sum;
+            let z = self.r_sum + s_sum;
+            self.result_output = self.x_sum + s_sum;
+            self.final_x = x;
+            self.final_z = z;
+
+            BigUint::from_bytes_le(&self.result_output.to_bytes()).to_u64_digits()[0]
+        }
     }
 
     // <===== Step 10 =====>
@@ -249,4 +332,44 @@ impl BinomialRunner {
     pub fn get_rhs(&self) -> String {
         BigUint::from_bytes_le(&self.rhs.compress().to_bytes()).to_str_radix(10)
     }
+}
+
+#[test]
+pub fn test_unbiased_p() {
+    let mut br: BinomialRunner = BinomialRunner::new(10, &[1, 1, 1, 0, 0, 1, 0, 1, 1, 1]);
+    let coms = br.get_x_commits();
+    br.input_randomness(&[1, 1, 0, 0, 1, 0]);
+    let privrand_coms = br.get_private_random_commits();
+    let pubrand = br.get_public_random();
+    let xorbits = br.get_xor_bits();
+    println!("XORed bits: {:?}", xorbits);
+    let xorcoms = br.get_xor_commits();
+    let output = br.compute_sum();
+    println!("Output: {}", output);
+    let z = br.get_z();
+    br.commit_pedersons();
+    let lhs = br.get_lhs();
+    let rhs = br.get_rhs();
+    assert_eq!(lhs, rhs)
+}
+
+#[test]
+pub fn test_biased_p() {
+    let mut br: BinomialRunner = BinomialRunner::new(10, &[1, 1, 1, 0, 0, 1, 0, 1, 1, 1]);
+    let coms = br.get_x_commits();
+    br.rand_p_init(6);
+    for _ in 0..6 {
+        assert!(br.random_variable_p_input(2, 5, &[1, 0, 1, 0, 0]));
+    }
+    assert!(br.random_variable_p_end());
+    let xorbits = br.get_xor_bits();
+    println!("XOR bits: {:?}", xorbits);
+    let xorcoms = br.get_xor_commits();
+    let out = br.compute_sum();
+    println!("XOR sum: {}", out);
+    let z = br.get_z();
+    br.commit_pedersons();
+    let lhs = br.get_lhs();
+    let rhs = br.get_rhs();
+    assert_eq!(lhs, rhs);
 }
